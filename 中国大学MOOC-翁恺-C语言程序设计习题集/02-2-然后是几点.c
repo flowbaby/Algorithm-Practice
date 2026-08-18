0c49646bea71537a6527a6845d0ba384a419c5b6
flowbaby <linhongtao2005@gmail.com>
2026-08-18 19:27:36 +0800

    第三题

---
+++ 中国大学MOOC-翁恺-C语言程序设计习题集/02-2-然后是几点.c
@@ -0,0 +1,44 @@
+// 时间限制
+// 400 ms
+// 内存限制
+// 65536 kB
+// 代码长度限制
+// 8000 B
+// 判题程序
+// Standard
+// 作者
+// 翁恺（浙江大学）
+// 有时候人们用四位数字表示一个时间，比如1106表示11点零6分。现在，你的程序要根据起始时间和流逝的时间计算出终止时间。读入两个数字，第一个数字以这样的四位数字表示当前时间，第二个数字表示分钟数，计算当前时间经过那么多分钟后是几点，结果也表示为四位数字。当小时为个位数时，没有前导的零，即5点30分表示为530。注意，第二个数字表示的分钟数可能超过60，也可能是负数。
+
+// 输入格式：
+
+// 输入在一行中给出2个整数，分别是四位数字表示的起始时间、以及流逝的分钟数，其间以空格分隔。注意：在起始时间中，当小时为个位数时，没有前导的零，即5点30分表示为530；流逝的分钟数可能超过60，也可能是负数。
+
+// 输出格式：
+
+// 输出四位数字表示的终止时间。题目保证起始时间和终止时间在同一天内。
+
+// 输入样例：
+// 1120 110
+// 输出样例：
+// 1310
+
+#include <stdio.h>
+int main()
+{
+    int initTime, initHours, initMinute, totalTime, addTime, hours, minute;
+
+    scanf("%d %d", &initTime, &addTime);
+    initHours = initTime / 100;
+    initMinute = initTime % 100;
+
+    totalTime = initHours * 60 + initMinute;
+    totalTime += addTime;
+    hours = totalTime / 60;
+    minute = totalTime % 60;
+    printf("%d\n", hours);
+    printf("%d\n", minute);
+    printf("%d", hours * 100 + minute);
+
+    return 0;
+}
