ca01984a1b4f5b73903cced8b0aff33160dc1d53
flowbaby <linhongtao2005@gmail.com>
2026-09-16 22:31:32 +0800

    添加混合类型数据格式化输入程序实现，顺序读取并输出浮点数、整数和字符

--- 中国大学MOOC-翁恺-C语言程序设计习题集/06-0. 混合类型数据格式化输入(5).c
+++ 中国大学MOOC-翁恺-C语言程序设计习题集/06-0. 混合类型数据格式化输入(5).c
@@ -26,7 +26,11 @@
 
 int main()
 {
-    
+    double f1, f2;
+    int i;
+    char c;
+    scanf("%lf %d %c %lf", &f1, &i, &c, &f2);
+    printf("%c %d %.2f %.2f", c, i, f1, f2);
 
     return 0;
 }
